054a50b9483bd9159aab1f65ea04ac0f381effff
zen <zenon.deviagge@gmail.com>
2024-02-12 22:19:54 -0300

    navbar icons refactored to JSX to avoid code repetition

--- src/data.js
+++ src/data.js
@@ -20,3 +20,21 @@ export const pageLinks = [
     text: 'tours',
   },
 ]
+
+export const socialLinks = [
+  {
+    id: 1,
+    href: 'https://www.twitter.com',
+    icon: 'fab fa-facebook',
+  },
+  {
+    id: 2,
+    href: 'https://www.twitter.com',
+    icon: 'fab fa-twitter',
+  },
+  {
+    id: 3,
+    href: 'https://www.twitter.com',
+    icon: 'fab fa-squarespace',
+  },
+]
