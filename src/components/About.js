8adf8706588cab8e6480d08330bd43d8517c7b27
zen <zenon.deviagge@gmail.com>
2024-02-13 23:26:58 -0300

    images added to Header and About components

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,5 @@
+import aboutImg from '../images/about.jpeg'
+
 const About = () => {
   return (
     <section className="section" id="about">
@@ -10,7 +12,7 @@ const About = () => {
       <div className="section-center about-center">
         <div className="about-img">
           <img
-            src="./images/about.jpeg"
+            src={aboutImg}
             className="about-photo"
             alt="awesome beach"
           />
