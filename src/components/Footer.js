23924b0a8f8fbab4a5b3eb8947de79e1f03712ac
zen <zenon.deviagge@gmail.com>
2024-02-15 19:47:31 -0300

    Footer component refactored

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,41 @@
+import { footerLinks } from '../data'
+import { footerIcons } from '../data'
+
 const Footer = () => {
   return (
     <footer class="section footer">
       <ul class="footer-links">
-        <li>
-          <a href="#home" class="footer-link">
-            home
-          </a>
-        </li>
-        <li>
-          <a href="#about" class="footer-link">
-            about
-          </a>
-        </li>
-        <li>
-          <a href="#services" class="footer-link">
-            services
-          </a>
-        </li>
-        <li>
-          <a href="#featured" class="footer-link">
-            featured
-          </a>
-        </li>
+        {footerLinks.map((link) => {
+          const { id, content } = link
+          return (
+            <li key={id}>
+              <a href={`#${content}`} class="footer-link">
+                {content}
+              </a>
+            </li>
+          )
+        })}
       </ul>
       <ul class="footer-icons">
-        <li>
-          <a href="https://www.twitter.com" target="_blank" class="footer-icon" rel="noreferrer">
-            <i class="fab fa-facebook"></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" class="footer-icon" rel="noreferrer">
-            <i class="fab fa-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" class="footer-icon" rel="noreferrer">
-            <i class="fab fa-squarespace"></i>
-          </a>
-        </li>
+        {footerIcons.map((pageIcon) => {
+          const { id, icon } = pageIcon
+          return (
+            <li key={id}>
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                class="footer-icon"
+                rel="noreferrer"
+              >
+                <i class={icon}></i>
+              </a>
+            </li>
+          )
+        })}
       </ul>
       <p class="copyright">
         copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
   )
--- src/data.js
+++ src/data.js
@@ -1,3 +1,11 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+import tour5 from './images/tour-5.jpeg'
+import tour6 from './images/tour-6.jpeg'
+
+
 export const pageLinks = [
   {
     id: 1,
@@ -43,4 +51,81 @@ export const services = [
   {id: 1, icon: 'fas fa-wallet fa-fw', title: 'saving money', text: 'Lorem ipsum dolor sit amet consectetur'},
   {id: 2, icon: 'fas fa-tree fa-fw', title: 'endless hiking', text: 'Lorem ipsum dolor sit amet consectetur'},
   {id: 3, icon: 'fas fa-socks fa-fw', title: 'amazing comfort', text: 'Lorem ipsum dolor sit amet consectetur'},
+]
+
+export const tours = [
+  {
+    id: 1,
+    img: tour1,
+    date: 'august 26th, 2024',
+    title: 'Tibet Adventure',
+    info: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis',
+    location: 'china',
+    duration: '6 days',
+    cost: 'from $2100',
+  },
+  {
+    id: 2,
+    img: tour2,
+    date: 'october 1th, 2024',
+    title: 'best of java',
+    info: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis',
+    location: 'indonesia',
+    duration: '11 days',
+    cost: 'from $1400',
+  },
+  {
+    id: 3,
+    img: tour3,
+    date: 'september 15th, 2024',
+    title: 'explore hong kong',
+    info: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis',
+    location: 'hong kong',
+    duration: '8 days',
+    cost: 'from $5000',
+  },
+  {
+    id: 4,
+    img: tour4,
+    date: 'december 5th, 2024',
+    title: 'kenya highlights',
+    info: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis',
+    location: 'kenya',
+    duration: '20 days',
+    cost: 'from $3300',
+  },
+  {
+    id: 5,
+    img: tour5,
+    date: 'january 25th, 2024',
+    title: 'the best of ethiopia',
+    info: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis',
+    location: 'ethiopia',
+    duration: '12 days',
+    cost: 'from $6000',
+  },
+  {
+    id: 6,
+    img: tour6,
+    date: 'march 1th, 2024',
+    title: 'incredible india',
+    info: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis',
+    location: 'india',
+    duration: '14 days',
+    cost: 'from $2900',
+  } 
+
+]
+
+export const footerLinks = [
+  { id: 1, content: 'home'},
+  { id: 2, content: 'about'},
+  { id: 3, content: 'services'},
+  { id: 4, content: 'tours'},
+]
+
+export const footerIcons = [
+  {id:1, icon: 'fab fa-facebook'},
+  {id:2, icon: 'fab fa-twitter'},
+  {id:3, icon: 'fab fa-squarespace'},
 ]
